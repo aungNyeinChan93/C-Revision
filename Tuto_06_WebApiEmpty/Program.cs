e9d8e3a7b794945c221c3f2278e208b3cb946753
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-08 10:33:01 +0630

    Feat: minial api books

--- Tuto_06_WebApiEmpty/Program.cs
+++ Tuto_06_WebApiEmpty/Program.cs
@@ -1,5 +1,5 @@
-using DatabaseThree.Models;
 using DatabaseTwo.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,19 +7,70 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/databaseTwo", () =>
-{
-    DatabaseTwo.Models.AppDbContext db = new DatabaseTwo.Models.AppDbContext();
-    var books = db.Books.AsNoTracking().Where(b => b.DeleteFlag == false).ToList();
-    return books;
-});
+//app.MapGet("/databaseTwo", () =>
+//{
+//    DatabaseTwo.Models.AppDbContext db = new DatabaseTwo.Models.AppDbContext();
+//    var books = db.Books.AsNoTracking().Where(b => b.DeleteFlag == false).ToList();
+//    return books;
+//});
 
-app.MapGet("/databaseThree", () =>
+//app.MapGet("/databaseThree", () =>
+//{
+//    DatabaseThree.Models.AppDbContext _db = new DatabaseThree.Models.AppDbContext();
+//    var blogs = _db.Blogs.AsNoTracking().ToList();
+//    return blogs;
+//});
+
+app.MapGet("/api/books", () =>
 {
-    DatabaseThree.Models.AppDbContext _db = new DatabaseThree.Models.AppDbContext();
-    var blogs = _db.Blogs.AsNoTracking().ToList();
-    return blogs;
-});
+    AppDbContext _db = new AppDbContext();
+    var books = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false).ToList();
+    return books.Count >=1 ? Results.Ok(books) :Results.NotFound();
+}).WithName("GetAllBooks");
+
+app.MapGet("/api/books/{id}", ([FromRoute]int id) =>
+{
+    AppDbContext _db = new AppDbContext();
+    var book = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false && b.BookId == id).FirstOrDefault();
+    return book is not null ? Results.Ok(book) : Results.NotFound();
+}).WithName("GetBook");
+
+app.MapPost("/api/books", (Book book) =>
+{
+    AppDbContext _db = new AppDbContext();
+    _db.Add(book);
+    var result = _db.SaveChanges();
+    return result >= 1 ? Results.Ok("Create Success") : Results.BadRequest();
+}).WithName("CreateBook");
+
+app.MapPut("/api/books/{id}", (Book book, [FromRoute] int id) =>
+{
+    AppDbContext _db = new AppDbContext();
+    var b = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false && b.BookId == id).FirstOrDefault();
+    if (b is null)
+    {
+        return Results.BadRequest();
+    }
+    b.Title = book.Title;
+    b.Author = book.Author;
+    b.Year = book.Year;
+
+    _db.Entry(b).State = EntityState.Modified;
+    var res = _db.SaveChanges();
+    return res >= 1 ? Results.Created() : Results.BadRequest();
+
+}).WithName("UpdateBook");
+
+app.MapDelete("/api/books/{id}", ([FromRoute]int id) =>
+{
+    AppDbContext _db = new AppDbContext();
+    var b = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false && b.BookId == id).FirstOrDefault();
+    if (b is null) return Results.BadRequest();
+    b.DeleteFlag = true;
+    _db.Entry(b).State = EntityState.Modified;
+    var result = _db.SaveChanges();
+    return result >= 1 ? Results.NoContent() : Results.BadRequest();
+}).WithName("DeleteBook");
 
 app.Run();
 
