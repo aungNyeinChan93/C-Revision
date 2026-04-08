279127df707bd3ee302ffad9e3acb0e7debb8b0e
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-08 11:00:57 +0630

    Feat: file separate by use extension method

---
+++ Tuto_06_WebApiEmpty/EndPoints/BookEndPoint.cs
@@ -0,0 +1,66 @@
+﻿using DatabaseTwo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tuto_06_MinimalApi.EndPoints
+{
+    public static class BookEndPoint
+    {
+
+        // extension method 
+        public static void UseBook(this IEndpointRouteBuilder app )
+        {
+
+            app.MapGet("/api/books", () =>
+            {
+                AppDbContext _db = new AppDbContext();
+                var books = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false).ToList();
+                return books.Count >= 1 ? Results.Ok(books) : Results.NotFound();
+            }).WithName("GetAllBooks");
+
+            app.MapGet("/api/books/{id}", ([FromRoute] int id) =>
+            {
+                AppDbContext _db = new AppDbContext();
+                var book = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false && b.BookId == id).FirstOrDefault();
+                return book is not null ? Results.Ok(book) : Results.NotFound();
+            }).WithName("GetBook");
+
+            app.MapPost("/api/books", (Book book) =>
+            {
+                AppDbContext _db = new AppDbContext();
+                _db.Add(book);
+                var result = _db.SaveChanges();
+                return result >= 1 ? Results.Ok("Create Success") : Results.BadRequest();
+            }).WithName("CreateBook");
+
+            app.MapPut("/api/books/{id}", (Book book, [FromRoute] int id) =>
+            {
+                AppDbContext _db = new AppDbContext();
+                var b = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false && b.BookId == id).FirstOrDefault();
+                if (b is null)
+                {
+                    return Results.BadRequest();
+                }
+                b.Title = book.Title;
+                b.Author = book.Author;
+                b.Year = book.Year;
+
+                _db.Entry(b).State = EntityState.Modified;
+                var res = _db.SaveChanges();
+                return res >= 1 ? Results.Created() : Results.BadRequest();
+
+            }).WithName("UpdateBook");
+
+            app.MapDelete("/api/books/{id}", ([FromRoute] int id) =>
+            {
+                AppDbContext _db = new AppDbContext();
+                var b = _db.Books.AsNoTracking().Where(b => b.DeleteFlag == false && b.BookId == id).FirstOrDefault();
+                if (b is null) return Results.BadRequest();
+                b.DeleteFlag = true;
+                _db.Entry(b).State = EntityState.Modified;
+                var result = _db.SaveChanges();
+                return result >= 1 ? Results.NoContent() : Results.BadRequest();
+            }).WithName("DeleteBook");
+        }
+    }
+}
---
+++ Tuto_06_WebApiEmpty/Tests/TestForExtension.cs
@@ -0,0 +1,14 @@
+﻿using DatabaseThree.Models;
+using DatabaseTwo.Models;
+
+namespace Tuto_06_MinimalApi.Tests
+{
+    public static class TestForExtension
+    {
+        public static string Test(this Book book)
+        {
+            book.Title = "Test";
+            return $"Title =>> {book?.Title}";
+        }
+    }
+}
